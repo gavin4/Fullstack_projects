306004960b71e073481225bea3b79190bdc05895
Gavin Balakrishnan <gavin4@pdx.edu>
2019-04-22 18:04:30 -0700

    finished node task #1

--- node/1-lorem.js
+++ node/1-lorem.js
@@ -2,4 +2,14 @@
 
 var http = require('http'); // do not change this line
 
+const server = http.createServer(function(req,res){
+	res.writeHead( 200, {
+		'Content-Type': 'text/html'
+	});
+	res.write('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+	res.end();
+});
+
+console.log('Server is running');
+server.listen(process.env.PORT || 8080);
 // any request should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
