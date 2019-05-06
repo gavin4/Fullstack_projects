8c7a4876d66fb8cf33a7e50368e1397f90847d21
Gavin Balakrishnan <gavin4@pdx.edu>
2019-05-06 16:01:50 -0700

    finished express task #1

--- express/1-lorem.js
+++ express/1-lorem.js
@@ -2,4 +2,26 @@
 
 var express = require('express'); // do not change this line
 
+
+var server = express();
+
+server.get('/',function(req, res){
+	res.status(200);
+	res.set({
+		'Content-Type': 'text/html'
+	});
+	res.send('root');
+
+});
+
+server.get('/lorem',function(req, res){
+	res.status(200);
+	res.set({
+		'Content-Type': 'text/html'
+	});
+	res.send('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+
+});
+
+server.listen(process.env.PORT||8080);
 // http://localhost:8080/lorem should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
